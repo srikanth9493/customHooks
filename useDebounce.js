c6e9459dd7ef9002366e552e7cd3deb5f859c23c
srikanth9493 <srikanth9949839402@gmail.com>
2021-08-15 16:21:17 +0530

    added useDebounce hook

--- useDebounce.js
+++ useDebounce.js
@@ -1,1 +1,16 @@
+import {useEffect, useState} from "react";
+
+const useDebounce = (value, delay) => {
+  const [searchtext, setsearchtext] = useState(value);
+  useEffect(() => {
+    const handler = setTimeout(() => setsearchtext(value), delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [delay, value]);
+
+  return searchtext;
+};
+
+export default useDebounce;
 
